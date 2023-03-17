3d508f7ff859fb0215c8d9d9866c69bc2c27bc60
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-17 16:30:50 +0000

    prayers times, names

--- qibla/src/Components/prayers.jsx
+++ qibla/src/Components/prayers.jsx
@@ -1,98 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Prayers() {
-    //First we create new states to get the geo-coordinations of the use
+    //First, we create new states to get the geo-coordinations of the use
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
-
-  //Second we need to initialize the variables that will store the prayers times 
-  const [fajr, setFajr] = useState();
-  const [dhuhr, setDhuhr] = useState();
-  const [asr, setAsr] = useState();
-  const [maghrib, setMaghrib] = useState();
-  const [isha, setIsha] = useState();
-
-  //Third we need the current year, month, day and the hidjri date
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1;
   const currentDate = new Date().getDate();
   const date = new Date();
   const options = { day: "numeric", month: "long", year: "numeric" };
   const formattedDate = date.toLocaleDateString("en-US", options);
+
+  //Second, we need to initialize the variables that will store the prayers times 
+  const [prayers, setPrayers] = useState([ {prayerName:'Fajr', prayerTime:''}, {prayerName:'Sunrise', prayerTime:''}, {prayerName:'Dhuhr', prayerTime:''}, 
+  {prayerName:'Asr', prayerTime:''}, {prayerName:'Maghrib', prayerTime:''}, {prayerName:'Isha', prayerTime:''}])
+
+  //Third, we set the Hidjri date and the current city states
   const [hidjriDate, setHidjriDate] = useState("");
+  const [currentCity, setCurrentCity] = useState();
 
-
-  //Fourth we need to ask the user for his location 
-  function getLocation() {
+  //Fourth, we need to ask the user for his location 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
-  }
 
-  //Fifth the getLocation() function will call showPosition to store the geo-coordinates
   function showPosition(position) {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
   }
 
-  //Sixth we need to execute the getLocation() function
-  getLocation();
+  //Fifth, using the axios function, we will fetch the data from the api using the variables that we initialized in the first step
+  useEffect(() => {
+    if (latitude && longitude) {
 
-  //Seventh, using the axios function, we will fetch the data from the api using the variables that we initialized in the first step
-  function todayPrayers() {
-    const url = `https://api.aladhan.com/v1/calendar/${currentYear}/${currentMonth}?latitude=${latitude}&longitude=${longitude}&method=3`;
+      const url = `https://api.aladhan.com/v1/calendar/${currentYear}/${currentMonth}?latitude=${latitude}&longitude=${longitude}&method=3`;
 
-    axios.get(url).then((response) => {
-      setFajr(response.data.data[currentDate].timings.Fajr);
-      setDhuhr(response.data.data[currentDate].timings.Dhuhr);
-      setAsr(response.data.data[currentDate].timings.Asr);
-      setMaghrib(response.data.data[currentDate].timings.Maghrib);
-      setIsha(response.data.data[currentDate].timings.Isha);
-      setHidjriDate(
-        response.data.data[currentDate].date.hijri.day +
-          " " +
-          response.data.data[currentDate].date.hijri.month.en +
-          " " +
-          response.data.data[currentDate].date.hijri.year
-      );
-    });
-  }
+      axios.get(url).then((response) => {
+        const updatedPrayers = [...prayers];
+        updatedPrayers[0].prayerTime = response.data.data[currentDate].timings.Fajr;
+        updatedPrayers[1].prayerTime = response.data.data[currentDate].timings.Sunrise;
+        updatedPrayers[2].prayerTime = response.data.data[currentDate].timings.Dhuhr;
+        updatedPrayers[3].prayerTime = response.data.data[currentDate].timings.Asr;
+        updatedPrayers[4].prayerTime = response.data.data[currentDate].timings.Maghrib;
+        updatedPrayers[5].prayerTime = response.data.data[currentDate].timings.Isha;
+        setPrayers(updatedPrayers);
+        setHidjriDate(
+          response.data.data[currentDate].date.hijri.day +
+            " " +
+            response.data.data[currentDate].date.hijri.month.en +
+            " " +
+            response.data.data[currentDate].date.hijri.year
+        );
+      });
 
-  todayPrayers();
+      //Sixth, now, we are going to use the user's geocoordinates and another api to get the name of his current city
+      const url2 = `https://api.opencagedata.com/geocode/v1/json?key=093d474da6624e9a84d74236f69f6c2a&q=${latitude}%2C${longitude}&pretty=1`;
+      axios.get(url2).then((response) => {
+        setCurrentCity(response.data.results[0].components.city);
+      });
+    }
+  }, [latitude, longitude]);
 
   return (
     <div className="Container">
       <div id='top'>
-      <h2>Prayer Times in Porto</h2>
+      <h2>Prayer Times in {currentCity}</h2>
       <div id="dates">
         <p id="gregorian">{formattedDate}</p>
-        <p id="hidjri">{hidjriDate}</p>
+        <b id="hidjri">{hidjriDate}</b>
       </div>
       </div>
       <div id='main'>
-        <div className="prayer">
-          <h3>Fajr</h3>
-          <h3>{fajr}</h3>
-        </div>
-        <div className="prayer">
-          <h3>Dhuhr</h3>
-          <h3>{dhuhr}</h3>
-        </div>
-        <div className="prayer">
-          <h3>Asr</h3>
-          <h3>{asr}</h3>
-        </div>
-        <div className="prayer">
-          <h3>Maghrib</h3>
-          <h3>{maghrib}</h3>
-        </div>
-        <div className="prayer">
-          <h3>Isha</h3>
-          <h3>{isha}</h3>
-        </div>
+        {prayers.map((prayer, index)=>(
+            <div className="prayer" key={index}>
+                <h3>{prayer.prayerName}</h3>
+                <p>{prayer.prayerTime}</p>
+            </div>
+        ))}
       </div>
       <div id='bottom'>
           <h5>Muslim World League</h5>
