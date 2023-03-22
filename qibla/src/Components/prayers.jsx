b00d06b7289c334fdcbff3e405e2330cc32759bc
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-22 04:37:00 +0000

    next prayer

--- qibla/src/Components/prayers.jsx
+++ qibla/src/Components/prayers.jsx
@@ -1,53 +1,61 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from 'moment';
 
 function Prayers() {
-    //First, we create new states to get the geo-coordinations of the use
+
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth() + 1;
-  const currentDate = new Date().getDate();
+
+  const [prayers, setPrayers] = useState([
+    {name:'Fajr', time:''},
+    {name:'Sunrise', time:''},
+    {name:'Dhuhr', time:''},
+    {name:'Asr', time:''},
+    {name:'Maghrib', time:''},
+    {name:'Isha', time:''}
+  ])
+  const [nextPrayer, setNextPrayer] = useState({name:'Fajr', time:''});
+
+  const [hidjriDate, setHidjriDate] = useState("");
+  const [currentCity, setCurrentCity] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState('');
+
   const date = new Date();
   const options = { day: "numeric", month: "long", year: "numeric" };
   const formattedDate = date.toLocaleDateString("en-US", options);
 
-  //Second, we need to initialize the variables that will store the prayers times 
-  const [prayers, setPrayers] = useState([ {prayerName:'Fajr', prayerTime:''}, {prayerName:'Sunrise', prayerTime:''}, {prayerName:'Dhuhr', prayerTime:''}, 
-  {prayerName:'Asr', prayerTime:''}, {prayerName:'Maghrib', prayerTime:''}, {prayerName:'Isha', prayerTime:''}])
-
-  //Third, we set the Hidjri date and the current city states
-  const [hidjriDate, setHidjriDate] = useState("");
-  const [currentCity, setCurrentCity] = useState();
-
-  //Fourth, we need to ask the user for his location 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentDate = new Date().getDate();
+  
+  function getLocationData() {
+    navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition): console.log("Geolocation is not supported by this browser.");
+  }
+  
   function showPosition(position) {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
   }
 
-  //Fifth, using the axios function, we will fetch the data from the api using the variables that we initialized in the first step
   useEffect(() => {
+    getLocationData();
+  }, []);
+  
+
+  function getPrayersData() {
     if (latitude && longitude) {
-
       const url = `https://api.aladhan.com/v1/calendar/${currentYear}/${currentMonth}?latitude=${latitude}&longitude=${longitude}&method=3`;
-
       axios.get(url).then((response) => {
         const updatedPrayers = [...prayers];
-        updatedPrayers[0].prayerTime = response.data.data[currentDate].timings.Fajr.replace(' (WET)', '');
-        updatedPrayers[1].prayerTime = response.data.data[currentDate].timings.Sunrise.replace(' (WET)', '');
-        updatedPrayers[2].prayerTime = response.data.data[currentDate].timings.Dhuhr.replace(' (WET)', '');
-        updatedPrayers[3].prayerTime = response.data.data[currentDate].timings.Asr.replace(' (WET)', '');
-        updatedPrayers[4].prayerTime = response.data.data[currentDate].timings.Maghrib.replace(' (WET)', '');
-        updatedPrayers[5].prayerTime = response.data.data[currentDate].timings.Isha.replace(' (WET)', '');
+        updatedPrayers[0].time = response.data.data[currentDate].timings.Fajr.replace(' (WET)', '');
+        updatedPrayers[1].time = response.data.data[currentDate].timings.Sunrise.replace(' (WET)', '');
+        updatedPrayers[2].time = response.data.data[currentDate].timings.Dhuhr.replace(' (WET)', '');
+        updatedPrayers[3].time = response.data.data[currentDate].timings.Asr.replace(' (WET)', '');
+        updatedPrayers[4].time = response.data.data[currentDate].timings.Maghrib.replace(' (WET)', '');
+        updatedPrayers[5].time = response.data.data[currentDate].timings.Isha.replace(' (WET)', '');
         setPrayers(updatedPrayers);
-        nextPrayer(prayers)
+        setNextPrayer(updatedPrayers[0]);
         setHidjriDate(
           response.data.data[currentDate].date.hijri.day +
             " " +
@@ -57,35 +65,70 @@ function Prayers() {
         );
       });
 
-      //Sixth, now, we are going to use the user's geocoordinates and another api to get the name of his current city
       const url2 = `https://api.opencagedata.com/geocode/v1/json?key=093d474da6624e9a84d74236f69f6c2a&q=${latitude}%2C${longitude}&pretty=1`;
       axios.get(url2).then((response) => {
         setCurrentCity(response.data.results[0].components.city);
       });
     }
-  }, [latitude, longitude]);
-  const currentTime = new Date();
-const formattedTime = currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+  }
 
-//function to see what is the next prayer
-function nextPrayer(prayers) {
+ useEffect(() => {
+  getPrayersData()
+  }, [longitude, latitude] );
+
+  function getNextPrayer(prayers) {
+    const updatedPrayers = prayers;
     const now = new Date();
-    const prayerTimes = prayers.map(prayer => {
-      const timeParts = prayer.prayerTime.split(':');
-      const prayerTime = new Date();
-      prayerTime.setHours(parseInt(timeParts[0]), parseInt(timeParts[1]), 0, 0);
-      return { name: prayer.prayerName, time: prayerTime };
+  
+    const prayerTimes = updatedPrayers.map(prayer => {
+      const timeParts = prayer.time.split(':');
+      const time = new Date();
+      time.setHours(parseInt(timeParts[0]), parseInt(timeParts[1]), 0, 0);
+      return { name: prayer.name, time: time };
     });
+  
     prayerTimes.sort((a, b) => a.time - b.time);
     const nextPrayerTime = prayerTimes.find(prayer => prayer.time > now);
+    
     if (nextPrayerTime) {
-      return nextPrayerTime.name;
+      const updatedNextPrayer = {name: nextPrayerTime.name, time: nextPrayerTime.time.toLocaleTimeString()};
+      setNextPrayer(updatedNextPrayer);
     } else {
-      return prayers[0].prayerName
+      const updatedNextPrayer = {name: prayers[0].name, time: prayers[0].time};
+      setNextPrayer(updatedNextPrayer);
     }
   }
   
+  useEffect(() => {
+    getNextPrayer(prayers)
+  }, [prayers]);
+
+  function NextPrayerCountdown(nextPrayer) {
+    const today = moment().format('YYYY-MM-DD');
+    const targetTime = moment(`${today} ${nextPrayer.time}`);
+    const intervalId = setInterval(() => {
+      const currentTime = moment();
+      const diff = Math.max(0, targetTime.diff(currentTime));
+      const duration = moment.duration(diff);
+      if (duration.asMilliseconds() <= 0) {
+        clearInterval(intervalId);
+        return;
+      }
+      const hours = Math.floor(duration.asHours()).toString().padStart(2, '0');
+      const minutes = duration.minutes().toString().padStart(2, '0');
+      const seconds = duration.seconds().toString().padStart(2, '0');
+      const timeString = `${hours}:${minutes}:${seconds}`;
+      setTimeRemaining(timeString);
+    }, 1000);
   
+    return () => clearInterval(intervalId);
+  }
+  
+
+  useEffect(() => {
+    NextPrayerCountdown(nextPrayer)
+  }, [nextPrayer]);
+
   return (
     <div className="Container">
       <div id='top'>
@@ -97,10 +140,11 @@ function nextPrayer(prayers) {
       </div>
       <div id='main'>
         {prayers.map((prayer, index)=>(
-            <div className="prayer" key={index} id={nextPrayer(prayers)===prayer.prayerName? 'nextPrayer': null}  style={{ backgroundImage:nextPrayer(prayers)===prayer.prayerName && `url("./img/${  prayer.prayerName}.png")`}}>
-                <h3>{prayer.prayerName}</h3>
-                <p>{prayer.prayerTime}</p>
-                {nextPrayer(prayers)===prayer.prayerName? <h5>UPCOMING PRAYER</h5>: null}
+             <div className="prayer" key={index} id={nextPrayer.name===prayer.name? 'nextPrayer': null}  style={{ backgroundImage:nextPrayer.name===prayer.name && `url("./img/${prayer.name}.png")`}}>
+                <h3>{prayer.name}</h3>
+                <p>{prayer.time}</p>
+                {nextPrayer.name===prayer.name? <h5>UPCOMING PRAYER</h5>: null}
+                {nextPrayer.name===prayer.name? <h3>{timeRemaining}</h3>: null}
             </div>
         ))}
       </div>
@@ -112,4 +156,3 @@ function nextPrayer(prayers) {
 }
 
 export default Prayers;
-
