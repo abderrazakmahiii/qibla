57886a26a01fc707c288773bc180a203af6516c0
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-27 03:01:48 +0100

    final

--- qibla/src/Components/prayers.jsx
+++ qibla/src/Components/prayers.jsx
@@ -30,7 +30,7 @@ function Prayers() {
 
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1;
-  const currentDate = new Date().getDate();
+  const currentDate = new Date().getDate() -1;
   
   function getLocationData() {
     navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition): console.log("Geolocation is not supported by this browser.");
@@ -51,23 +51,23 @@ function Prayers() {
       const url = `https://api.aladhan.com/v1/calendar/${currentYear}/${currentMonth}?latitude=${latitude}&longitude=${longitude}&method=3`;
       axios.get(url).then((response) => {
         const updatedPrayers = [...prayers];
-        updatedPrayers[0].time = response.data.data[currentDate].timings.Fajr.replace(' (WET)', '');
-        updatedPrayers[1].time = response.data.data[currentDate].timings.Sunrise.replace(' (WET)', '');
-        updatedPrayers[2].time = response.data.data[currentDate].timings.Dhuhr.replace(' (WET)', '');
-        updatedPrayers[3].time = response.data.data[currentDate].timings.Asr.replace(' (WET)', '');
-        updatedPrayers[4].time = response.data.data[currentDate].timings.Maghrib.replace(' (WET)', '');
-        updatedPrayers[5].time = response.data.data[currentDate].timings.Isha.replace(' (WET)', '');
+        updatedPrayers[0].time = response.data.data[currentDate].timings.Fajr.slice(0,5);
+        updatedPrayers[1].time = response.data.data[currentDate].timings.Sunrise.slice(0,5);
+        updatedPrayers[2].time = response.data.data[currentDate].timings.Dhuhr.slice(0,5);
+        updatedPrayers[3].time = response.data.data[currentDate].timings.Asr.slice(0,5);
+        updatedPrayers[4].time = response.data.data[currentDate].timings.Maghrib.slice(0,5);
+        updatedPrayers[5].time = response.data.data[currentDate].timings.Isha.slice(0,5);
         setPrayers(updatedPrayers);
         setNextPrayer(updatedPrayers[0]);
         setHidjriDate(
           response.data.data[currentDate].date.hijri.day +
-            " " +
-            response.data.data[currentDate].date.hijri.month.en +
-            " " +
-            response.data.data[currentDate].date.hijri.year
+          " " +
+          response.data.data[currentDate].date.hijri.month.en +
+          " " +
+          response.data.data[currentDate].date.hijri.year
         );
       });
-
+  
       const url2 = `https://api.opencagedata.com/geocode/v1/json?key=093d474da6624e9a84d74236f69f6c2a&q=${latitude}%2C${longitude}&pretty=1`;
       axios.get(url2).then((response) => {
         setCurrentCity(response.data.results[0].components.city);
@@ -75,6 +75,7 @@ function Prayers() {
       });
     }
   }
+  
 
  useEffect(() => {
   getPrayersData()
@@ -151,14 +152,20 @@ function Prayers() {
         </div>
         </div>
         <div id='main'>
-          {prayers.map((prayer, index)=>(
-               <div className="prayer" key={index} id={nextPrayer.name===prayer.name? 'nextPrayer': null}  style={{ backgroundImage:nextPrayer.name===prayer.name && `url("./img/${prayer.name}.png")`}}>
-                  <h3>{prayer.name}</h3>
-                  <p>{prayer.time}</p>
-                  {nextPrayer.name===prayer.name? <h5>UPCOMING PRAYER</h5>: null}
-                  {nextPrayer.name===prayer.name? <h3>{timeRemaining}</h3>: null}
-              </div>
-          ))}
+        {prayers.map((prayer, index)=>(
+  <div className="prayer" key={index} id={nextPrayer.name===prayer.name? 'nextPrayer': null} style={{ backgroundImage:nextPrayer.name===prayer.name && `url("./img/${prayer.name}.png")`}}>
+    <div className="left" style={{ width: (nextPrayer.name !== prayer.name && window.innerWidth <= 900) ? '100%' : 'auto', display: (nextPrayer.name !== prayer.name && window.innerWidth <= 900) ? 'flex' : 'block', justifyContent: (nextPrayer.name !== prayer.name && window.innerWidth <= 900) ? 'space-between' : 'inherit', alignItems: (nextPrayer.name !== prayer.name && window.innerWidth <= 900) ? 'center' : 'inherit', textAlign: (nextPrayer.name !== prayer.name && window.innerWidth <= 900) ? 'center' : 'inherit' }}>
+      <h3>{prayer.name}</h3>
+      <p style={{ fontSize: (nextPrayer.name !== prayer.name) ? '18px' : 'inherit', fontWeight: (nextPrayer.name !== prayer.name) ? '600' : 'inherit' }}>{prayer.time}</p>
+    </div>
+    <div className="right" style={{ textAlign: (window.innerWidth <= 900)? 'right' : 'center'}}>
+      {nextPrayer.name===prayer.name? <h5>UPCOMING PRAYER</h5>: null}
+      {nextPrayer.name===prayer.name?  <h3>{timeRemaining}</h3>: null}
+    </div>
+  </div>
+))}
+
+
         </div>
         <div id='bottom'>
             <h5>Muslim World League</h5>
@@ -176,4 +183,5 @@ else {
   )
 }
 }
+
 export default Prayers;
